d807df3ee54da7ef2bc249c111e5f4b4395148bb
Gustave Michel III <gus3@michelfamily.org>
2013-12-09 10:11:43 -0600

    Added Documentation Comments

--- Relay.h
+++ Relay.h
@@ -1,20 +1,62 @@
-#ifndef Relay_h
+/**
+ * Relay.h
+ * Purpose: Operate a 5v Relay.
+ * 
+ * @author <a href="mailto:gus3@michelfamily.org">Gustave A. Michel III</a>
+ * @version 1.0 12/07/13
+ * @license The GNU Pulic Liscense v3
+ */
+ #ifndef Relay_h
 #define Relay_h
 
 #include "Arduino.h"
 
 class Relay {
   public:
+    /**
+     * Empty constructor, an init() method must be called before instance use
+     * @since v1.0
+     */
     Relay();
+    /**
+     * Constructor, defaults starting State
+     * @param pin Relay Control Pin
+     * @since v1.0
+     */
     Relay(uint8_t pin);
+    /**
+     * Constructor
+     * @param pin Relay Control Pin
+     * @param state Starting State for Relay
+     * @since v1.0
+     */
     Relay(uint8_t pin, bool state);
+    /**
+     * Initializes Relay after empty constructor, defaults starting state
+     * @param pin Relay Control Pin
+     * @since v1.0
+     */
     void init(uint8_t pin);
+    /**
+     * Initializes Relay after empty constructor, defaults starting state
+     * @param pin Relay Control Pin
+     * @param state Starting State for Relay
+     * @since v1.0
+     */
     void init(uint8_t pin, bool state);
+    /**
+     * Sets state of Relay
+     * @since v1.0
+     */
     void set(bool state);
+    /**
+     * Get current state of Relay
+     * @return State of Relay
+     */
     bool get();
   private:
-    bool _init, _state;
-    uint8_t _pin;
+    bool _init, _state; //_init: If the object has been setup; _state current state of relay, defaults to false
+    uint8_t _pin; //_pin: Pin of Relay
 };
 
 #endif
--- Salinometer.h
+++ Salinometer.h
@@ -1,22 +1,68 @@
-#ifndef Salinometer_h
+/**
+ * Salinometer.h
+ * Purpose: Wrapper for Voltage Divider Salinity Meter
+ * 
+ * @author <a href="mailto:gus3@michelfamily.org">Gustave A. Michel III</a>
+ * @version 1.0 12/07/13
+ * @license The GNU Pulic Liscense v3
+ */
+ #ifndef Salinometer_h
 #define Salinometer_h
 
 #include "Arduino.h"
 
 class Salinometer {
   public:
+    /**
+     * Empty constructor, an init() method must be called before instance use
+     * @since v1.0
+     */
     Salinometer();
+    /**
+     * Constructor, doesn't allow use of is_in_tolerance().
+     * @param pin Analog Pin of sensor
+     * @since v1.0
+     */
     Salinometer(uint8_t pin);
+    /**
+     * Constructor
+     * @param pin Analog Pin of sensor
+     * @param lower_limit Lower limit calibration
+     * @param upper_limit Upper limit calibration
+     * @since v1.0
+     */
     Salinometer(uint8_t pin, uint16_t lower_limit, uint16_t upper_limit);
+    /** 
+     * Initializes Salinometer after empty constructor, doesn't allow use of is_in_tolerance().
+     * @param pin Analog Pin of sensor
+     * @since v1.0
+     */
     void init(uint8_t pin);
+    /**
+     * Initializes Salinometer after empty constructor.
+     * @param pin Analog Pin of sensor
+     * @param lower_limit Lower limit calibration
+     * @param upper_limit Upper limit calibration
+     */
     void init(uint8_t pin, uint16_t lower_limit, uint16_t upper_limit);
+    /**
+     * Returns Analog Value of sensor
+     * @return Value from 0 to 1023
+     */
     uint16_t get_value();
+    /**
+     * Checks if the value of the sensor is betweent the upper and lower limits
+     * @return if the value is between the limits
+     */
     bool is_in_tolerance();
+    /**
+     * Sets the calibration limits, allows use of is_in_tolerance()
+     */
     void set_limits(uint16_t lower_limit, uint16_t upper_limit);
   private:
-    uint8_t _pin;
-    uint16_t _lower_limit, _upper_limit;
-    bool _init, _limits;
+    uint8_t _pin; //_pin: Analog Pin
+    uint16_t _lower_limit, _upper_limit; //_lower_limit: Lower, smaller value limit for calibration; _upper_limit: Upper, larger value limit for calibration;
+    bool _init, _limits; //_init: if object has been initialized; _limits: if is_in_tolerance() can be used;
 };
 
 #endif
